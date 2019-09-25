876dea7a210e90fc6335a58a5a1a07cbf269c0ce
Joshua Sharp <sharp.josh@gmail.com>
2019-09-25 00:57:25 -0400

    Implemented Login/Registration with CoreData only

---
+++ ChoreTracker/ChoreTracker/Resources/ErrorHandling.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorHandling.swift
+//  ChoreTracker
+//
+//  Created by Joshua Sharp on 9/24/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import Foundation
+
+enum AppError: String, Error, Equatable {
+    case objectToRepFailed = "Failed to translate object to Representative."
+    case nameNotUnique = "The username provided is already taken."
+}
