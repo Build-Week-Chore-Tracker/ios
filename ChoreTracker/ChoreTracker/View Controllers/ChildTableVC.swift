1c41d884f6b11f35c3c0a649cc441d43007a09cf
Percy <percival.ngan@gmail.com>
2019-09-26 14:05:23 -0500

    Added code to ChildChoreDetailVC

--- ChoreTracker/ChoreTracker/View Controllers/ChildTableVC.swift
+++ ChoreTracker/ChoreTracker/View Controllers/ChildTableVC.swift
@@ -31,7 +31,7 @@ class ChildTableVC: UITableViewController {
 											 sectionNameKeyPath: "choreTemplate.name",
 											 cacheName: nil)
 
-		frc.delegate = self as? NSFetchedResultsControllerDelegate
+		frc.delegate = self as NSFetchedResultsControllerDelegate
 
 		do {
 			try frc.performFetch()
@@ -42,87 +42,132 @@ class ChildTableVC: UITableViewController {
 		return frc
 	}()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		tableView.reloadData()
+	}
 
 	// MARK: - Methods
 
 
 
-    // MARK: - Table view data source
+	// MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return fetchResultsController.sections?.count ?? 0
+	}
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return fetchResultsController.sections?[section].numberOfObjects ?? 0
+	}
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ChoreCell", for: indexPath)
 
-        return cell
-    }
-    */
+		let chore = fetchResultsController.object(at: indexPath)
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+		cell.textLabel?.text = chore.choreTemplate?.name
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+		return cell
+	}
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 
-    }
-    */
+		guard let sectionInfo = fetchResultsController.sections?[section] else { return nil }
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+		return sectionInfo.name.capitalized
+	}
 
-    /*
-    // MARK: - Navigation
+	/*
+	// Override to support conditional editing of the table view.
+	override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+	// Return false if you do not want the specified item to be editable.
+	return true
+	}
+	*/
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+	// Override to support editing the table view.
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			// Delete the row from the data source
+			let chore = fetchResultsController.object(at: indexPath)
+
+			choreController.delete(chore: chore)
+		}
+
+		// MARK: - Navigation
+
+		// In a storyboard-based application, you will often want to do a little preparation before navigation
+		func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+			// Get the new view controller using segue.destination.
+			if segue.identifier == "ChildChoreSegue" {
+				guard let detailVC = segue.destination as? ChildChoreDetailVC, let indexPath = tableView.indexPathForSelectedRow else { return }
+
+				let chore = fetchResultsController.object(at: indexPath)
+
+				detailVC.chore = chore
+				detailVC.choreController = choreController
+			}
+		}
+	}
 }
 
+extension ChildTableVC: NSFetchedResultsControllerDelegate {
 
+	func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+		tableView.beginUpdates()
+	}
+
+	func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+		tableView.endUpdates()
+	}
+
+	func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+					didChange anObject: Any,
+					at indexPath: IndexPath?,
+					for type: NSFetchedResultsChangeType,
+					newIndexPath: IndexPath?) {
+		switch type {
+		case .insert:
+			guard let newIndexPath = newIndexPath else { return }
+			tableView.insertRows(at: [newIndexPath], with: .automatic)
+		case .delete:
+			guard let indexPath = indexPath else { return }
+			tableView.deleteRows(at: [indexPath], with: .automatic)
+		case .move:
+			guard let indexPath = indexPath,
+				let newIndexPath = newIndexPath else { return }
+			tableView.moveRow(at: indexPath, to: newIndexPath)
+		case .update:
+			guard let indexPath = indexPath else { return }
+			tableView.reloadRows(at: [indexPath], with: .automatic)
+		@unknown default:
+			return
+		}
+	}
+
+	func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+					didChange sectionInfo: NSFetchedResultsSectionInfo,
+					atSectionIndex sectionIndex: Int,
+					for type: NSFetchedResultsChangeType) {
+
+		let sectionSet = IndexSet(integer: sectionIndex)
+
+		switch type {
+		case .insert:
+			tableView.insertSections(sectionSet, with: .automatic)
+		case .delete:
+			tableView.deleteSections(sectionSet, with: .automatic)
+		default:
+			return
+		}
+	}
+}
