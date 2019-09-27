cbc5c0aabfb4263c9caa63a785cd40e7369dc877
Joshua Sharp <sharp.josh@gmail.com>
2019-09-27 14:53:44 -0400

    Auto stash before merge of "Development" and "origin/Development"

--- ChoreTracker/ChoreTracker/View Controllers/AdultChoreDetailVC.swift
+++ ChoreTracker/ChoreTracker/View Controllers/AdultChoreDetailVC.swift
@@ -15,6 +15,7 @@ class AdultChoreDetailVC: UIViewController {
     
     var chore: Chore?
     var choreController: ChoreController?
+    
     lazy var frc: NSFetchedResultsController<ChoreTemplate>? = {
         let fetchRequest: NSFetchRequest<ChoreTemplate> = ChoreTemplate.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
@@ -32,28 +33,61 @@ class AdultChoreDetailVC: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
 		view.backgroundColor = .loginBackground
-        // Do any additional setup after loading the view.
+        childPicker.delegate = self
+        childPicker.dataSource = self
     }
 
 	override func viewWillAppear(_ animated: Bool) {
 		navigationItem.title = "Chores"
 	}
     
-
-    private func updateViews() {
-        
+    @IBAction func savetapped(_ sender: Any) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var descriptionText: UITextView!
+    @IBOutlet weak var notesText: UITextView!
+    @IBOutlet weak var pointsText: UILabel!
+    @IBOutlet weak var pictureSwitch: UISwitch!
+    @IBOutlet weak var childPicker: UIPickerView!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    
+    
+    var childrenList: [User] = {
+        let context = CoreDataStack.shared.mainContext
+        let fetchRequest: NSFetchRequest<User> = User.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        guard let currentUser = UserController.currentUser else { return [] }
+        //print ("childrenList: CurrentUser = \(String(describing: currentUser))")
+        fetchRequest.predicate = NSPredicate(format: "parent == %@", currentUser)
+        do {
+            let children = try context.fetch(fetchRequest)
+            //print ("childrenList: Children = \(String(describing: children))")
+            return children
+        } catch {
+            NSLog("uniqueLoginName: Error fetching children for picker")
+            return []
+        }
+    } ()
+    
+    
+    private func updateViews(_ choreTemplate: ChoreTemplate? = nil) {
+        if let choreTemplate = choreTemplate {
+            //Setup from ChoreTemplate
+            descriptionText.text = choreTemplate.choreDescription
+            notesText.text = choreTemplate.notes
+            let pointsString = String(choreTemplate.points)
+            pointsText.text = pointsString
+            if let assignedUser = choreTemplate.assignedUser {
+                
+            }
+            
+    //            if childPicker.selectedRow(inComponent: 0) != 0 {
+    //                assignedChild = childrenList[childPicker.selectedRow(inComponent: 0)-1]
+    //                print ("Saving assigned child as \(String(describing: assignedChild))")
+    //            }
+            
+        }
     }
-    */
-
 }
 
 extension AdultChoreDetailVC: UITableViewDataSource {
@@ -73,7 +107,7 @@ extension AdultChoreDetailVC: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChoreTemplateCell", for: indexPath)
         let choreTemplate = frc.object(at: indexPath)
         cell.textLabel?.text = choreTemplate.name
-        cell.detailTextLabel?.text = choreTemplate.description
+        cell.detailTextLabel?.text = choreTemplate.choreDescription
         return cell
     }
     
@@ -87,13 +121,35 @@ extension AdultChoreDetailVC: UITableViewDataSource {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let vc = segue.destination as? AdultChoreTemplateDetailVC else { return }
+        guard let frc = frc else { return }
+        switch segue.identifier {
+        case "ChoreTemplateDetailSegue":
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let choreTemplate = frc.object(at: indexPath)
+                vc.choreTemplate = choreTemplate
+                NSLog("AdultChoreDetailVC: Sending choreTemplate: \(String(describing: choreTemplate))")
+            }
+        case "AddChoreSegue":
+            break
+        default:
+            break
+        }
+    }
 }
 
 
 extension AdultChoreDetailVC: UITableViewDelegate {
  
     private func tableView(_ tableView: UITableView, didSelectRowAt: IndexPath) {
-        
+        guard let frc = frc else { return }
+        let choreTemplate = frc.object(at: didSelectRowAt)
+        updateViews(choreTemplate)
     }
 }
 
@@ -143,6 +199,31 @@ extension AdultChoreDetailVC: NSFetchedResultsControllerDelegate {
             tableView.deleteRows(at: [indexPath], with: .automatic)
         @unknown default:
             fatalError("Unknown Change Type occured")
+        }
+    }
+}
+
+extension AdultChoreDetailVC: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return childrenList.count + 1
+    }
+    
+    
+}
+
+extension AdultChoreDetailVC: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //print ("Doing Child Picker...")
+        if row == 0 {
+            //print ("row 0...None")
+            return "None"
+        } else {
+            //print ("other rows... \(childrenList[row-1].name ?? "Empty")")
+            return childrenList[row-1].name ?? ""
         }
     }
 }
